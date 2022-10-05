1dab6231d3aafca42d487c0c823936a5bb34c7ad
iamvarunnair <varun.n@spurtreetech.in>
2022-10-05 22:19:49 +0530

    input to add texts to list

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+**0.1.0**
+Added a text filed that takes input and adds to a list. List items if exceed a const char limit, exceeded parts are highlighted. List items can be removed.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+import { useRef, useState, Fragment, createElement } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+function createMarkup(value) {
+    return { __html: '<strong>' + value + '</strong>' };
+}
+
+const HILIGHT_TEXT_AFTER_CHAR = 5;
+
+const Highlight = ({ string, indexHighlight = null }) =>
+    indexHighlight && indexHighlight > -1 && string.length > indexHighlight ? (
+        <>
+            {string.slice(0, indexHighlight)}
+            <mark>{string.slice(indexHighlight, string.length)}</mark>
+        </>
+    ) : (
+        <>{string}</>
+    );
+
+const TabText = ({
+    value,
+    onRemove = null,
+    indexRemove = null,
+    highlightLimit = null,
+}) => (
+    <p
+        style={{
+            padding: '2px 10px',
+            marginRight: '4px',
+            marginBottom: '2px',
+            borderRadius: '50px',
+            backgroundColor: '#cccccc',
+        }}
+    >
+        <Highlight string={value} indexHighlight={highlightLimit} />{' '}
+        <strong
+            style={{ marginLeft: '2px', fontFamily: 'sans' }}
+            onClick={() => onRemove(indexRemove)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            x
+        </strong>
+    </p>
+);
+
+function App() {
+    const [input, setInput] = useState('');
+    const [list, setList] = useState([]);
+    const onKey = (e) => {
+        console.log('vbotxi ', e);
+        if (e.key === 'Enter' || e.code === 'Enter' || e.keyCode === 13) {
+            setList([...list, e.target.value]);
+            setInput('');
+        }
+    };
+    const onChange = (e) => {
+        if (
+            e.target &&
+            (e.target.value !== null || e.target.value !== undefined)
+        ) {
+            /* because e.target.value === '' when we remove text from input, we still need the setInput to work */
+            setInput(e.target.value);
+        }
+    };
+    const onRemove = (index) => {
+        if (index !== null || index !== undefined) {
+            const newArray = [...list];
+            newArray.splice(index, 1);
+            setList([...newArray]);
+        }
+    };
+    return (
+        <div className='App'>
+            {list.map((el, index) => (
+                <TabText
+                    value={el}
+                    onRemove={onRemove}
+                    indexRemove={index}
+                    highlightLimit={HILIGHT_TEXT_AFTER_CHAR}
+                />
+            ))}
+            <input
+                className='custom-width'
+                value={input}
+                onChange={onChange}
+                onKeyUp={onKey}
+            />
+        </div>
+    );
 }
 
 export default App;
